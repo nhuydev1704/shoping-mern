742fda42aa524ed8f4ca45928dc6f8b0ca3e331f
nguyennhuy1704 <khanhandli@gmail.com>
2021-08-18 23:31:29 +0700

    update

--- controllers/notificationCtrl.js
+++ controllers/notificationCtrl.js
@@ -24,7 +24,7 @@ class APIfeatures {
 	paginating() {
 		const page = this.queryString.page * 1 || 1
 		const limit = this.queryString.limit * 1 || 8
-		skip = (page - 1) * limit;
+		const skip = (page - 1) * limit;
 		this.query = this.query.skip(skip).limit(limit)
 
 		return this;
